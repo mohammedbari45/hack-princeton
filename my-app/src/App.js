96ef5e632f0ce0c442b1cdbea4776e9eb4065144
notrojo22 <rosanroan@gmail.com>
2024-11-09 05:43:17 -0500

    commits

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,8 +7,10 @@ function App() {
     <div className="App">
       <Header />
       <main>
-        
         <h2>Because credit can be confusing.</h2>
+        <h5> Explore new credit card options: 
+          
+        </h5>
       </main>
     </div>
   );

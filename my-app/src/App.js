a2fa40a14fa5aa2ea6d8e099d7a9d6b9571d4d4c
mohammedbari45 <mbari1852@gmail.com>
2024-11-09 17:02:38 -0500

    Fixed header and added housing link

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './Header';
 import HomePage from './Homepage';
 import CashbackPage from './CashbackPage';
+import HousingPage from './HousingPage'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/cashback" element={<CashbackPage />} />
+          <Route path="/housingpage" element={<HousingPage />} />
         </Routes>
       </div>
     </Router>
--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -14,7 +14,6 @@ function Header() {
         <li><Link to="/expenses">Expenses</Link></li>
           <li><Link to="/cashback">Cashback</Link></li>
           <li><Link to="/housing">Housing</Link></li>
-          <li><Link to="/restaurants">Restaurants</Link></li>
         </ul>
       </nav>
       <div className="profile-icon">

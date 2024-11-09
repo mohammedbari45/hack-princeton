e21d49e15ad8635195c85560162b0a4d338aa9a5
notrojo22 <rosanroan@gmail.com>
2024-11-09 08:19:49 -0500

    changes by rojas to heading

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,24 @@ import './App.css';
 import React from 'react';
 import Header from './Header';
 
+
+
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
         <h2>Because credit can be confusing.</h2>
-        <h5> Explore new credit card options: 
-          
-        </h5>
+        <h3>name is your all-in-one dashboard to manage and review your credit expenses and what they can offer.</h3>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h2>We offer expense tracking, personalized and relevant fetching of cashback, restaurant and housing recommendations</h2>
+        <img src = "https://thumbs.dreamstime.com/b/money-bag-16262524.jpg" alt="logo"></img>
       </main>
+      
     </div>
+    
   );
 }
 
